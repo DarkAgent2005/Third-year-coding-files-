b947b6d0c5fde1b9757768061e3ed5ae8b2dfea4
Prashant Sawant <sawantprashant501@gmail.com>
2025-09-18 10:30:00 +0530

    java basics

---
+++ _01_variables_and_datatypes/day_01/_04_basic.java
@@ -0,0 +1,11 @@
+public class _04_basic {
+    public static void main (String [] args){
+        int a = 5;
+        int b = 6;
+        int sum = a+b;
+        System.out.print(sum);
+        /*
+         this is the multi-line-comment 
+         */
+    }
+}
