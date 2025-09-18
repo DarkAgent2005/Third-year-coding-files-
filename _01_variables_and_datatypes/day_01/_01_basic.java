9540bcd369086ce6a2a5970322fa6ff8da4aacc2
Prashant Sawant <sawantprashant501@gmail.com>
2025-09-18 10:30:00 +0530

    java day 1

---
+++ _01_variables_and_datatypes/day_01/_01_basic.java
@@ -0,0 +1,11 @@
+
+public class _01_basic {
+    public static void main(String[] args) {
+        System.out.println("first line");
+        System.out.println("Second line");
+        System.out.println("Third line\nFourth");
+        System.out.println("Fifth line");
+    }
+   
+}
+  
