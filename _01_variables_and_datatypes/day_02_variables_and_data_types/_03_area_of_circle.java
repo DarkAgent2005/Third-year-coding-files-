773488c874859f308a164a2d4b66504dcc274a4f
Prashant Sawant <sawantprashant501@gmail.com>
2025-09-20 11:15:00 +0530

    area of circle question

---
+++ _01_variables_and_datatypes/day_02_variables_and_data_types/_03_area_of_circle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class _03_area_of_circle {
+     public static void main(String args[]) {
+     Scanner sc  = new Scanner(System.in);
+     System.out.print("Enter the value of the radius of circle:");
+     double a = sc.nextInt();
+     double area = 3.14*a*a;
+     System.out.print("The radius of circle is :" + area);
+     sc.close();
+     }
+     
+}
