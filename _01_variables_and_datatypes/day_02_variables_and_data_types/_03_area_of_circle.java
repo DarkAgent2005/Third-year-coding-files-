b69350fd18cad19d7f43b41175c0509fc1ef961c
Prashant Sawant <sawantprashant501@gmail.com>
2025-09-20 11:15:00 +0530

    just suppressed the warning

--- _01_variables_and_datatypes/day_02_variables_and_data_types/_03_area_of_circle.java
+++ _01_variables_and_datatypes/day_02_variables_and_data_types/_03_area_of_circle.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class _03_area_of_circle {
+     @SuppressWarnings("ConvertToTryWithResources")
      public static void main(String args[]) {
      Scanner sc  = new Scanner(System.in);
      System.out.print("Enter the value of the radius of circle:");
